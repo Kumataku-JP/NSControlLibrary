0c7d9fb1014602a3ad65e371ef628266fd570f42
Kumataku <61592773+Kumataku-JP@users.noreply.github.com>
2024-09-23 22:06:09 +0900

    Add files via upload

--- src/NSControlLibrary.cpp
+++ src/NSControlLibrary.cpp
@@ -1,13 +1,103 @@
-/* v1.1.1 */
+/* v1.2.0 */
 
 #include "NSControlLibrary.h"
 #include <math.h>
 
+/* ここからSwitchControlLibrary */
+#if defined(_USING_HID)
+
+static const uint8_t _hidReportDescriptor[] PROGMEM = {
+  0x05, 0x01,        //   USAGE_PAGE (Generic Desktop)
+  0x09, 0x05,        //   USAGE (Game Pad)
+  0xa1, 0x01,        //   COLLECTION (Application)
+  0x15, 0x00,        //   LOGICAL_MINIMUM (0)
+  0x25, 0x01,        //   LOGICAL_MAXIMUM (1)
+  0x35, 0x00,        //   PHYSICAL_MINIMUM (0)
+  0x45, 0x01,        //   PHYSICAL_MAXIMUM (1)
+  0x75, 0x01,        //   REPORT_SIZE (1)
+  0x95, 0x10,        //   REPORT_COUNT (16)
+  0x05, 0x09,        //   USAGE_PAGE (Button)
+  0x19, 0x01,        //   USAGE_MINIMUM (1)
+  0x29, 0x10,        //   USAGE_MAXIMUM (16)
+  0x81, 0x02,        //   INPUT (Data,Var,Abs)
+  0x05, 0x01,        //   USAGE_PAGE (Generic Desktop)
+  0x25, 0x07,        //   LOGICAL_MAXIMUM (7)
+  0x46, 0x3b, 0x01,  //   PHYSICAL_MAXIMUM (315)
+  0x75, 0x04,        //   REPORT_SIZE (4)
+  0x95, 0x01,        //   REPORT_COUNT (1)
+  0x65, 0x14,        //   UNIT (20)
+  0x09, 0x39,        //   USAGE (Hat Switch)
+  0x81, 0x42,        //   INPUT (Data,Var,Abs)
+  0x65, 0x00,        //   UNIT (0)
+  0x95, 0x01,        //   REPORT_COUNT (1)
+  0x81, 0x01,        //   INPUT (Cnst,Arr,Abs)
+  0x26, 0xff, 0x00,  //   LOGICAL_MAXIMUM (255)
+  0x46, 0xff, 0x00,  //   PHYSICAL_MAXIMUM (255)
+  0x09, 0x30,        //   USAGE (X)
+  0x09, 0x31,        //   USAGE (Y)
+  0x09, 0x32,        //   USAGE (Z)
+  0x09, 0x35,        //   USAGE (Rz)
+  0x75, 0x08,        //   REPORT_SIZE (8)
+  0x95, 0x04,        //   REPORT_COUNT (4)
+  0x81, 0x02,        //   INPUT (Data,Var,Abs)
+  0x06, 0x00, 0xff,  //   USAGE_PAGE (Vendor Defined 65280)
+  0x09, 0x20,        //   USAGE (32)
+  0x95, 0x01,        //   REPORT_COUNT (1)
+  0x81, 0x02,        //   INPUT (Data,Var,Abs)
+  0x0a, 0x21, 0x26,  //   USAGE (9761)
+  0x95, 0x08,        //   REPORT_COUNT (8)
+  0x91, 0x02,        //   OUTPUT (Data,Var,Abs)
+  0xc0               // END_COLLECTION
+};
+
+SwitchControlLibrary_::SwitchControlLibrary_() {
+  static HIDSubDescriptor node(_hidReportDescriptor, sizeof(_hidReportDescriptor));
+  CustomHID().AppendDescriptor(&node);
+
+  memset(&_joystickInputData, 0, sizeof(USB_JoystickReport_Input_t));
+  _joystickInputData.LX = Stick::NEUTRAL;
+  _joystickInputData.LY = Stick::NEUTRAL;
+  _joystickInputData.RX = Stick::NEUTRAL;
+  _joystickInputData.RY = Stick::NEUTRAL;
+  _joystickInputData.Hat = Hat::NEUTRAL;
+}
+
+void SwitchControlLibrary_::sendReport() {
+  CustomHID().SendReport(&_joystickInputData, sizeof(USB_JoystickReport_Input_t));
+}
+void SwitchControlLibrary_::pressButton(uint16_t button) {
+  _joystickInputData.Button |= button;
+}
+void SwitchControlLibrary_::releaseButton(uint16_t button) {
+  _joystickInputData.Button &= (button ^ 0xffff);
+}
+void SwitchControlLibrary_::pressHatButton(uint8_t hat) {
+  _joystickInputData.Hat = hat;
+}
+void SwitchControlLibrary_::releaseHatButton() {
+  _joystickInputData.Hat = Hat::NEUTRAL;
+}
+void SwitchControlLibrary_::moveLeftStick(uint8_t lx, uint8_t ly) {
+  _joystickInputData.LX = lx;
+  _joystickInputData.LY = ly;
+}
+void SwitchControlLibrary_::moveRightStick(uint8_t rx, uint8_t ry) {
+  _joystickInputData.RX = rx;
+  _joystickInputData.RY = ry;
+}
+SwitchControlLibrary_ &SwitchControlLibrary() {
+  static SwitchControlLibrary_ obj;
+  return obj;
+}
+
+#endif
+
+/* ここからNSControlLibrary */
 // 度からラジアンに変換するヘルパー関数
-#define MY_DEG_TO_RAD(angleDegrees) ((angleDegrees) * 0.017453292519943295769236907684886)
+#define MY_DEG_TO_RAD(angleDegrees) ((angleDegrees)*0.017453292519943295769236907684886)
 
 uint8_t scaleValue(int8_t value, int8_t fromLow, int8_t fromHigh, uint8_t toLow, uint8_t toHigh) {
-    return (value - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
+  return (value - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
 }
 /*  ボタンを押して離す
  *
@@ -64,7 +154,7 @@ void pushHat(uint8_t hat, unsigned long input_time, unsigned long delay_time, un
     SwitchControlLibrary().releaseHatButton();
     SwitchControlLibrary().sendReport();
     delay(delay_time);
-    }
+  }
 }
 
 /*  十字キーを押し続ける
@@ -79,7 +169,7 @@ void holdHat(uint8_t hat, unsigned long delay_time) {
 }
 /*  十字キーを離す
  *
- *  @param uint8_t      button     離すボタン
+ *  @param uint8_t       button     離すボタン
  *  @param unsigned long delay_time ボタンを離した後の待ち時間(ms)
  */
 void releaseHat(unsigned long delay_time) {
@@ -92,56 +182,40 @@ void releaseHat(unsigned long delay_time) {
 /*  スティックを動かす
  *  方向を度数法(0〜360)、強さを0〜100の範囲で指定する
  *
+ *  @param uint8_t       stick       動かすスティック
  *  @param int16_t       angle_deg   方向を度数法で指定（0°は上）
  *  @param uint8_t       strength    スティックの強さを0〜100で指定
  *  @param unsigned long delay_time  待ち時間(ms、デフォルトは0)
  *
  */
+void stickTilt(uint8_t stick, int16_t angle_deg, uint8_t strength, unsigned long delay_time) {
+  double angle_rad = MY_DEG_TO_RAD(angle_deg - 90); // 0°が上（北）になるように調整
+  int16_t x = cos(angle_rad) * strength; // 強度をかけてX座標に変換
+  int16_t y = sin(angle_rad) * strength; // 強度をかけてY座標に変換
 
- //  左スティック
-void leftStickTilt(int16_t angle_deg, uint8_t strength, unsigned long delay_time) {
-    double adjusted_angle_deg = angle_deg - 90; // 0°が北になるように調整
-    double angle_rad = MY_DEG_TO_RAD(adjusted_angle_deg);
-    int8_t lx = (int8_t)(cos(angle_rad) * strength);
-    int8_t ly = (int8_t)(sin(angle_rad) * strength);
-    
-    uint8_t scaledLX = map(lx, -100, 100, 0, 255);
-    uint8_t scaledLY = map(ly, -100, 100, 0, 255);
-
-    SwitchControlLibrary().moveLeftStick(scaledLX, scaledLY);
-    SwitchControlLibrary().sendReport();
-    delay(delay_time);
-}
-
-//  右スティック
-void rightStickTilt(int16_t angle_deg, uint8_t strength, unsigned long delay_time) {
-    double adjusted_angle_deg = angle_deg - 90;
-    double angle_rad = MY_DEG_TO_RAD(adjusted_angle_deg);
-    int8_t rx = (int8_t)(cos(angle_rad) * strength);
-    int8_t ry = (int8_t)(sin(angle_rad) * strength);
-
-    uint8_t scaledRX = map(rx, -100, 100, 0, 255);
-    uint8_t scaledRY = map(ry, -100, 100, 0, 255);
-
-    SwitchControlLibrary().moveRightStick(scaledRX, scaledRY);
-    SwitchControlLibrary().sendReport();
-    delay(delay_time);
-}
-
-/*  スティックを元に戻す
- *   
- *  @param unsigned long delay_time ボタンを離した後の待ち時間(ms)
- */
-
- //  左スティック
-void leftStickNeutral(unsigned long delay_time) {
-  SwitchControlLibrary().moveLeftStick(128, 128);
+  uint8_t stickX = map(x, -100, 100, Stick::MIN, Stick::MAX); // X座標をマッピング
+  uint8_t stickY = map(y, -100, 100, Stick::MIN, Stick::MAX); // Y座標をマッピング
+  if (stick == Stick::LEFT) {
+    SwitchControlLibrary().moveLeftStick(stickX, stickY);
+  } else if (stick == Stick::RIGHT) {
+    SwitchControlLibrary().moveRightStick(stickX, stickY);
+  }
   SwitchControlLibrary().sendReport();
   delay(delay_time);
 }
-//  右スティック
-void rightStickNeutral(unsigned long delay_time) {
-  SwitchControlLibrary().moveRightStick(128, 128);
+
+/*  スティックを元に戻す
+ *  
+ *  @param uint8_t       stick       戻すスティック
+ *  @param unsigned long delay_time ボタンを離した後の待ち時間(ms)
+ */
+
+void stickNeutral(uint8_t stick, unsigned long delay_time) {
+  if (stick == Stick::LEFT) {
+    SwitchControlLibrary().moveLeftStick(Stick::NEUTRAL, Stick::NEUTRAL);
+  } else if (stick == Stick::RIGHT) {
+    SwitchControlLibrary().moveRightStick(Stick::NEUTRAL, Stick::NEUTRAL);
+  }
   SwitchControlLibrary().sendReport();
   delay(delay_time);
 }
--- src/NSControlLibrary.h
+++ src/NSControlLibrary.h
@@ -1,16 +1,95 @@
+/* v1.2.0 */
+
 #pragma once
 
-#include "./SwitchControlLibrary/src/SwitchControlLibrary.h"
+/* ここからSwitchControlLibrary */
+#include "CustomHID.h"
 
+// ボタン定義
+struct Button {
+    static const uint16_t Y = 0x0001;
+    static const uint16_t B = 0x0002;
+    static const uint16_t A = 0x0004;
+    static const uint16_t X = 0x0008;
+    static const uint16_t L = 0x0010;
+    static const uint16_t R = 0x0020;
+    static const uint16_t ZL = 0x0040;
+    static const uint16_t ZR = 0x0080;
+    static const uint16_t MINUS = 0x0100;
+    static const uint16_t PLUS = 0x0200;
+    static const uint16_t LCLICK = 0x0400;
+    static const uint16_t RCLICK = 0x0800;
+    static const uint16_t HOME = 0x1000;
+    static const uint16_t CAPTURE = 0x2000;
+};
+
+// 十字キー定義
+struct Hat {
+    static const uint8_t UP = 0x00;
+    static const uint8_t UP_RIGHT = 0x01;
+    static const uint8_t RIGHT = 0x02;
+    static const uint8_t DOWN_RIGHT = 0x03;
+    static const uint8_t DOWN = 0x04;
+    static const uint8_t DOWN_LEFT = 0x05;
+    static const uint8_t LEFT = 0x06;
+    static const uint8_t UP_LEFT = 0x07;
+    static const uint8_t NEUTRAL = 0x08;
+};
+
+// スティック定義
+struct Stick {
+    static const uint8_t LEFT = 0;
+    static const uint8_t RIGHT = 1;
+    static const uint8_t MIN = 0;
+    static const uint8_t NEUTRAL = 128;
+    static const uint8_t MAX = 255;
+};
+
+// USB ジョイスティックレポート構造体
+typedef struct {
+    uint16_t Button;
+    uint8_t Hat;
+    uint8_t LX;
+    uint8_t LY;
+    uint8_t RX;
+    uint8_t RY;
+    uint8_t VendorSpec;
+} USB_JoystickReport_Input_t;
+
+// SwitchControlLibraryクラス
+class SwitchControlLibrary_ {
+private:
+    USB_JoystickReport_Input_t _joystickInputData;
+
+public:
+    SwitchControlLibrary_();
+
+    void sendReport();
+
+    void pressButton(uint16_t button);
+    void releaseButton(uint16_t button);
+
+    void pressHatButton(uint8_t hat);
+    void releaseHatButton();
+
+    void moveLeftStick(uint8_t lx, uint8_t ly);
+    void moveRightStick(uint8_t rx, uint8_t ry);
+};
+
+// SwitchControlLibraryのインスタンスを返す関数
+SwitchControlLibrary_ &SwitchControlLibrary();
+
+/* ここからNSControlLibrary */
+// ボタン操作関数
 void pushButton(uint16_t button, unsigned long input_time = 40, unsigned long delay_time = 40, unsigned int loop = 1);
 void holdButton(uint16_t button, unsigned long delay_time = 0);
 void releaseButton(uint16_t button, unsigned long delay_time = 0);
 
+// 十字キー操作関数
 void pushHat(uint8_t hat, unsigned long input_time = 40, unsigned long delay_time = 40, unsigned int loop = 1);
 void holdHat(uint8_t hat, unsigned long delay_time = 0);
 void releaseHat(unsigned long delay_time = 0);
 
-void leftStickTilt(int16_t angle_deg, uint8_t strength, unsigned long delay_time = 0);
-void rightStickTilt(int16_t angle_deg, uint8_t strength, unsigned long delay_time = 0);
-void leftStickNeutral(unsigned long delay_time = 0);
-void rightStickNeutral(unsigned long delay_time = 0);
+// スティック操作関数
+void stickTilt(uint8_t stick, int16_t angle_deg, uint8_t strength, unsigned long delay_time = 0);
+void stickNeutral(uint8_t stick, unsigned long delay_time = 0);
